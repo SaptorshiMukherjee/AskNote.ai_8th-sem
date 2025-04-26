0f699b96ab92d1ff629f40d0e4fd8dd659a2b7e4
Saptorshi <saptorshi91@gmail.com>
2025-04-26 16:22:06 +0530

    gihub pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Tell Vite to prefix URLs with your repo name on GH Pages:
+  base: mode === "production" ? "/ASKNOTE_AI_8TH-SEM/" : "/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
